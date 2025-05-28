81c4973750ac62f182ce399bd9c0eb8873d8e464
Jay Sharma <jatinr507@gmail.com>
2025-05-28 11:49:25 -0400

    Add Edit Profile to Directory sidebar

--- src/views/Directory.vue
+++ src/views/Directory.vue
@@ -20,6 +20,17 @@
               </svg>
             </div>
           </div>
+
+          <!-- Edit Profile Link -->
+          <router-link 
+            to="/profile" 
+            class="flex items-center gap-2 text-[#2E4172] hover:text-[#1E2B4A] font-medium"
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+            </svg>
+            Edit Profile
+          </router-link>
         </div>
 
         <!-- Main Content -->
